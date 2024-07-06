3b18c0ed0523ebcc0922923e5200b6dbaedd9aaf
Documenter.jl <documenter@juliadocs.github.io>
2024-07-06 00:33:15 +0000

    build based on d95e351

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#ShortFFTs.jl","page":"ShortFFTs.jl","title":"ShortFFTs.jl","text":"","category":"section"},{"location":"","page":"ShortFFTs.jl","title":"ShortFFTs.jl","text":"Modules = [ShortFFTs]","category":"page"}]
+}
